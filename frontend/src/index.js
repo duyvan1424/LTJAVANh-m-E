5f8907617e7b24fb24011b004d3af3ca636a4f44
lamtriphucUTH <2251120436@ut.edu.vn>
2024-09-27 08:09:32 +0700

    them router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LoginPage from './pages/loginpage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ frontend/src/pages/loginpage.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import '../styles/loginpage.css'
+
+const LoginPage = () => {
+
+
+    return (
+        <div>
+            Login
+        </div>
+    )
+
+}
+export default LoginPage;
