c9c5cd64a1c5c99b57803697bb6e67f5f2fa086c
lamtriphucUTH <2251120436@ut.edu.vn>
2024-09-27 17:34:40 +0700

    hallo

--- frontend/src/components/layout/footer.jsx
+++ frontend/src/components/layout/footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './footer.css'
 
-function Footer() {
+const Footer = () => {
   return (
     <div className='footer'>
       Footer
@@ -9,4 +9,4 @@ function Footer() {
   )
 }
 
-export default Footer
+export default Footer;
--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -1,33 +1,38 @@
 import '../styles/register.css'
+import { Link } from 'react-router-dom'
+import Header from '../components/layout/header';
 const RegisterPage = () => {
     return (
-        <div class="register-container">
-            <h2>Create Account</h2>
-            <form action="/register" method="post">
-                <div class="input-group">
-                    <label for="fullname">Full Name</label>
-                    <input type="text" id="fullname" name="fullname" required />
-                </div>
-                <div class="input-group">
-                    <label for="email">Email</label>
-                    <input type="email" id="email" name="email" required />
-                </div>
-                <div class="input-group">
-                    <label for="username">Username</label>
-                    <input type="text" id="username" name="username" required />
-                </div>
-                <div class="input-group">
-                    <label for="password">Password</label>
-                    <input type="password" id="password" name="password" required />
-                </div>
-                <div class="input-group">
-                    <label for="confirm-password">Confirm Password</label>
-                    <input type="password" id="confirm-password" name="confirm-password" required />
-                </div>
-                <button type="submit">Register</button>
-            </form> <br />
-            <p class="login-text">Already have an account? <a href="/login">Login here</a></p>
-        </div>
+        <>
+            <Header />
+            <div class="register-container">
+                <h2>Create Account</h2>
+                <form action="/register" method="post">
+                    <div class="input-group">
+                        <label for="fullname">Full Name</label>
+                        <input type="text" id="fullname" name="fullname" required />
+                    </div>
+                    <div class="input-group">
+                        <label for="email">Email</label>
+                        <input type="email" id="email" name="email" required />
+                    </div>
+                    <div class="input-group">
+                        <label for="username">Username</label>
+                        <input type="text" id="username" name="username" required />
+                    </div>
+                    <div class="input-group">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" name="password" required />
+                    </div>
+                    <div class="input-group">
+                        <label for="confirm-password">Confirm Password</label>
+                        <input type="password" id="confirm-password" name="confirm-password" required />
+                    </div>
+                    <button type="submit">Register</button>
+                </form> <br />
+                <p class="login-text">Already have an account? <Link to="/login">Login here</Link></p>
+            </div>
+        </>
     )
 }
 export default RegisterPage;
