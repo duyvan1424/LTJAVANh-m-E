84ee86e3a91445195774f82bcfd596e8c5429eab
lamtriphucUTH <2251120436@ut.edu.vn>
2024-10-02 10:07:32 +0700

    login-register

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,29 +1,37 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import '../styles/login.css'
 import Header from '../components/layout/header';
 
 const LoginPage = () => {
 
     return (
-        <>
-            <Header />
-            <div class="login-container">
-                <h2>Login</h2>
-                <form action="/login" method="post">
-                    <div class="input-group">
-                        <label for="username">Username</label>
-                        <input type="text" id="username" name="username" required />
+        <div className="container">
+            <div class="wrapper">
+                <form action="">
+                    <h1>Login</h1>
+                    <div class="input-box">
+                        <input type="text" placeholder="Username" required />
+                        <i class='bx bxs-user'></i>
                     </div>
-                    <div class="input-group">
-                        <label for="password">Password</label>
-                        <input type="password" id="password" name="password" required />
+                    <div class="input-box">
+                        <input type="password" placeholder="Password" required />
+                        <i class='bx bxs-lock-alt'></i>
                     </div>
-                    <button type="submit">Login</button>
-                </form>
-                <p class="register-text">Don't have an account? <Link to="/register">Register here</Link></p>
+                    <div class="remember-forgot">
+                        <label>
+                            <input type="checkbox" /> Remember me
+                        </label>
+                        <a href="#">Forgot password? </a>
+                    </div>
+                    <button type="submit" class="btn">Login</button>
+                    <div class="register-link">
+                        <p>Don't have an account? <NavLink to="/register">Register</NavLink></p>
+                    </div>
+                </form >
             </div>
-        </>
+
+        </div>
     )
 
 }
