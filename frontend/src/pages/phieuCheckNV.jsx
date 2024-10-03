c81225189b6ec1ea429ff592fb314bc97c93b462
Vinhle1804 <131293438+Vinhle1804@users.noreply.github.com>
2024-10-03 13:06:06 +0700

    Add phieuCheckNV.jsx and phieuguiKH.jsx

---
+++ frontend/src/pages/phieuCheckNV.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PhieuCheckNv() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PhieuCheckNv
---
+++ frontend/src/pages/phieuguiKH.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PhieuguiKH() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PhieuguiKH
