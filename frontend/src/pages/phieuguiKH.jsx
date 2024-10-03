b14f917cc62c2a76380c66f309d8f1848d5a1335
Vinhle1804 <131293438+Vinhle1804@users.noreply.github.com>
2024-10-03 13:35:44 +0700

    hmm ok

--- frontend/src/pages/phieuguiKH.jsx
+++ frontend/src/pages/phieuguiKH.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function PhieuguiKH() {
   return (
     <div>
-      
+     <h1> form để điền thông tin giao nhận, có ô tick chọn dịch vụ lấy/ giao hàng tận nơi, tính ra phí ở total,  có button để khi người gửi nhấn vào thì sẽ gửi thông tin đến cho nhân viên</h1> 
     </div>
   )
 }
