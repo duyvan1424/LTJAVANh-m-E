cdc10ac2230ae99189735f8234aa3ca6b458128b
lamtriphucUTH <2251120436@ut.edu.vn>
2024-10-09 10:22:18 +0700

    trag about us và component service

--- frontend/src/components/layout/product.jsx
+++ frontend/src/components/layout/product.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import './product.css'
+import ProductImage1 from '../../assets/product-1.png'
+import ProductImage2 from '../../assets/product-2.png'
 
 function Product() {
   return (
-    <div className='h-[500px] bg-orange-200'>
-      <h1>có 2 dịch vụ vận chuyển trong nước và vận chuyển ngoài nước, tạo ra 2 ô sản phẩm để người dùng click vô</h1>
+    <div className='product'>
+      <h2 className='main-title'>Dịch vụ Vận chuyển Cá Koi</h2>
+      <div className="product-container">
+
+        <div className='shipping-service'>
+          <div className="icon">
+            <img src={ProductImage1} alt="Ảnh xe tải" className='icon' />
+          </div>
+          <h3 className='service-title'>Vận chuyển trong nước</h3>
+          <p className='service-description'>Dịch vụ vận chuyển cá Koi an toàn và nhanh chóng đến mọi tỉnh thành trong Việt Nam.</p>
+          <button className='btn'>Tìm hiểu thêm</button>
+        </div>
+
+        <div className='shipping-service'>
+          <div className="icon">
+            <img src={ProductImage2} alt="Ảnh xe tải" className='icon' />
+          </div>
+          <h3 className='service-title'>Vận chuyển quốc tế</h3>
+          <p className='service-description'>Dịch vụ vận chuyển cá Koi đến các quốc gia trên toàn thế giới, đảm bảo an toàn và tuân thủ quy định.</p>
+          <button className='btn'>Tìm hiểu thêm</button>
+        </div>
+
+      </div>
     </div>
   )
 }
-
 export default Product
