ea5465c2f29b5e789a03db1333ebe9827ce71094
Vinhle1804 <131293438+Vinhle1804@users.noreply.github.com>
2024-10-10 13:53:02 +0700

    ok

---
+++ frontend/src/components/layout/stepper.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Stepper, Step, StepLabel } from "@mui/material";
+import "./stepper.css";
+
+const steps = ["đặt hàng", "giao hàng", "nhận hàng"];
+const stepCurrent = [1];
+
+const   MyStepper = () => {
+  return (
+    <div className="stepper">
+      <Stepper activeStep={0}>
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+    </div>
+  );
+};
+
+export default MyStepper;
