92a11d4799a3e5840bc34c7c63c427b8028618a3
lamtriphucUTH <2251120436@ut.edu.vn>
2024-10-14 18:16:50 +0700

    fix header

--- frontend/src/components/layout/header.jsx
+++ frontend/src/components/layout/header.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 import './header.css';
 import logo from "../../assets/logo_nonebackground.png"
 
 const Header = ({ scrollToProduct }) => {
+  const [menuOpen, setMenuOpen] = useState(false); // State để theo dõi trạng thái của menu
   const location = useLocation(); // Lấy thông tin vị trí hiện tại
   const navigate = useNavigate(); // Để điều hướng giữa các trang
 
@@ -17,26 +18,38 @@ const Header = ({ scrollToProduct }) => {
     }
   };
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen); // Đổi trạng thái mở/đóng menu
+  };
+
   return (
     <header className="header">
       <nav className="header-nav">
-        <NavLink to="/" className="nav-link">
-          <div className="header-nav-left">
-            <img src={logo} alt="Logo" />
-          </div>
+        <label for="" className="checkbtn" onClick={toggleMenu}>&#9776;</label>
+        <NavLink>
+          <img className="logo" src={logo} alt="" />
         </NavLink>
-        <div className="header-nav-mid">
+
+        <ul className={`nav-list ${menuOpen ? 'open' : ''}`}>
+          <li className="list-item"><NavLink className="nav-link" to="/">TRANG CHỦ</NavLink></li>
+          <li className="list-item"><NavLink className="nav-link" onClick={handleServiceClick}>DỊCH VỤ</NavLink></li>
+          <li className="list-item"><NavLink className="nav-link" to="/information">THÔNG TIN</NavLink></li>
+          <li className="list-item"><NavLink className="nav-link" to="/track-order">THEO DÕI ĐƠN HÀNG</NavLink></li>
+          <li className="list-item"><NavLink className="nav-link" to="/login">ĐĂNG NHẬP</NavLink></li>
+        </ul>
+
+
+        {/* <div className="header-nav-mid">
           <NavLink to="/" className="nav-link">TRANG CHỦ</NavLink>
-            <a  onClick={handleServiceClick}>DỊCH VỤ</a> {/* Gọi hàm xử lý click */}
+          <NavLink className="nav-link" onClick={handleServiceClick}>DỊCH VỤ</NavLink>
           <NavLink to="/information" className="nav-link">THÔNG TIN</NavLink>
-          <NavLink to="/track-order" className="nav-link">THEO DÕI ĐƠN HANG</NavLink>
+          <NavLink to="/track-order" className="nav-link">THEO DÕI ĐƠN HÀNG</NavLink>
         </div>
         <div className="header-nav-right">
           <NavLink to="/login" className="nav-link">ĐĂNG NHẬP</NavLink>
-        </div>
+        </div> */}
       </nav>
-    </header>
-
+    </header >
   )
 }
 
