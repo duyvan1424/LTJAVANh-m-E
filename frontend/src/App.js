bed5469a2f60e43925db7b1acfeb56647425378b
Nguyễn Hồ Tấn Lộc <2251120027@ut.edu.vn>
2024-11-23 07:30:09 +0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@
 import { Route, Routes } from "react-router-dom";
 import LoginPage from './pages/login';
 import RegisterPage from './pages/register';
-import TrackOrderPage from './pages/jack-order';
 import InformationPage from './pages/information';
 import Home from './pages/home';
 import ShippingServicePage from './pages/vanchuyennoidia';
@@ -11,23 +10,60 @@ import Vanchuyennoidia from './pages/vanchuyennoidia';
 import Vanchuyenquocte from './pages/vanchuyenquocte';
 import OrderHistory from './pages/OrderHistory';
 import UserManagement from './pages/userManagement';
+import Tracuu from "./pages/tracuudonhang";
+import CheckOrder from "./pages/checkOrder";
+import PrivateRoute from "./components/layout/privateRoute";
+import StaffOrderManagement from "./pages/staff";
+// import UserInfoPage from "./pages/UserInfo";
+import Taodonhang from "./components/taodonhang";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<LoginPage />} />
+      {/* <Route path="/user-info" element={<UserInfoPage />} /> */}
       <Route path="/information" element={<InformationPage />} />
       <Route path="/vanchuyennoidia" element={<ShippingServicePage />} />
       <Route path="/domestic-shipping" element={<Vanchuyennoidia />} />
       <Route path="/international-shipping" element={<Vanchuyenquocte />} />
-      <Route path="/track-order" element={<TrackOrderPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/quanly" element={<UserManagement/>} />
+      <Route path="/track-order" element={<Tracuu/>} />
+       {/* Route dành cho nhân viên với phân quyền */}
+       <Route
+        path="/staff"
+        element={
+          <PrivateRoute requiredRole={["1", "2"]}> {/* Chấp nhận cả role "1" và "2" */}
+            <StaffOrderManagement />
+          </PrivateRoute>
+        }
+      />
+
+      {/* Route quản lý người dùng dành cho role "2" */}
+      <Route
+        path="/quanly"
+        element={
+          <PrivateRoute requiredRole={["2"]}>
+            <UserManagement />
+          </PrivateRoute>
+        }
+      />
+
+      {/* Route kiểm tra đơn hàng */}
+      <Route
+        path="/check"
+        element={
+          <PrivateRoute requiredRole={["1", "2"]}> {/* Chấp nhận cả role "1" và "2" */}
+            <CheckOrder />
+          </PrivateRoute>
+        }
+      />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/phieugui" element={<PhieuguiKH/>}/>
       <Route path="/history" element={<OrderHistory/>}/>
-    
+      <Route path="/tracuu" element={<Tracuu/>}/>
+      <Route path="/taodonhang" element={<Taodonhang/>}/>
+      
+
 
     </Routes>
   );
