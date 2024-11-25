c08f70cfb5fff94070bd82fb51b4c4c0dfc323ca
Nguyễn Hồ Tấn Lộc <2251120027@ut.edu.vn>
2024-11-25 09:11:34 +0700

    Thêm chú thích @NotBlank để đảm bảo các trường bắt buộc phải có giá trị.Đảm bảo các thông báo xác thực rõ ràng hơn để cải thiện phản hồi cho người dùng.thêm xác thực cho các giá trị của trường role (có thể sử dụng Enum)

--- backend/DEMACIAAA/src/main/java/com/demaxia/DEMACIAAA/dto/Request/UserCreationRequest.java
+++ backend/DEMACIAAA/src/main/java/com/demaxia/DEMACIAAA/dto/Request/UserCreationRequest.java
@@ -1,69 +1,29 @@
 package com.demaxia.DEMACIAAA.dto.Request;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
-
-import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class UserCreationRequest {
+    @NotBlank(message = "USERNAME_REQUIRED")
     @Size(min = 3, message = "USERNAME_INVALID")
     private String username;
 
+    @NotBlank(message = "PASSWORD_REQUIRED")
     @Size(min = 8, message = "PASSWORD_INVALID")
     private String password;
 
-    @Email(message = "EMAIL_INVALID") // Kiểm tra định dạng email
+    @NotBlank(message = "EMAIL_REQUIRED")
+    @Email(message = "EMAIL_INVALID")
     private String email;
-//    private String firstname;
-//    private String lastname;
-//    private LocalDate dob;
+
+    @NotBlank(message = "ROLE_REQUIRED")
     private String role;
 
-
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public String getFirstname() {
-//        return firstname;
-//    }
-//
-//    public void setFirstname(String firstname) {
-//        this.firstname = firstname;
-//    }
-//
-//    public String getLastname() {
-//        return lastname;
-//    }
-//
-//    public void setLastname(String lastname) {
-//        this.lastname = lastname;
-//    }
-//
-//    public LocalDate getDob() {
-//        return dob;
-//    }
-//
-//    public void setDob(LocalDate dob) {
-//        this.dob = dob;
-//    }
-
-
+    // Consider adding validations or constraints for role values (e.g., using Enum)
 }
