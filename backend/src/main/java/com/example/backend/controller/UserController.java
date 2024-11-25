394e22777f044c9711da9f3ce048f6a3c627904e
lamtriphucUTH <2251120436@ut.edu.vn>
2024-11-25 17:47:21 +0700

    thêm usercontroller

---
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.example.backend.controller;
+
+import com.example.backend.dto.UserResponse;
+import com.example.backend.entity.User;
+import com.example.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable int id, @RequestBody User user) {
+        try {
+            User updatedUser = userService.updateUser(id, user);
+            return ResponseEntity.ok(updatedUser);
+        } catch (Exception e) {
+            // Lỗi
+        }
+    }
+
+}
